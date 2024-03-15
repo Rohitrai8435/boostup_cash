6c43dd468824eaffebefd832a5a20d892c1ef738
Rohitrai8435 <rohitrai6263@gmail.com>
2024-03-15 16:10:19 +0530

    first typescript

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.local.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor() {
+    console.log(process.env.MONGODB_URI); // Log the value of MONGODB_URI
+  }
+}
